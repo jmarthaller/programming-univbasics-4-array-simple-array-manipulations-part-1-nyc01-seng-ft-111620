761d447e1a357a25318fa65dcfe63b7d68cb801f
Jacob Marthaller <marthaller.jr@gmail.com>
2020-10-14 19:20:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -19,3 +19,9 @@ end
 def using_shift(array)
   return array.shift
 end
+
+def shift_with_args(array)
+  first_shifted = array.shift
+  second_shifted = array.shift
+  return [second_shifted, first_shifted]
+end
